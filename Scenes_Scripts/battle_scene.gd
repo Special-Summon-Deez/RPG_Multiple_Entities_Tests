4ffd3f1cbde647f79e9348476812a0ba48dc5e3d
Jose Ruiz <jruiz170.business@gmail.com>
2024-07-07 15:16:44 -0400

    Added functionality to spawn enemies. Currently only displays texture

--- Scenes_Scripts/battle_scene.gd
+++ Scenes_Scripts/battle_scene.gd
@@ -1,19 +1,42 @@
 extends Node2D
 
 @export var battleSet: PackedScene
+
+@export var enemyArray: Array[enemy]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var enemies := load("res://enemy_resources/bruh_enemy.tres") as enemy
-	var enemyBody = battleSet.instantiate() as enemy_scene
-	enemyBody.setEnemy(enemies)	
-	add_child(enemyBody)
-	
-	enemies = load("res://enemy_resources/Bean.tres")
-	enemyBody = battleSet.instantiate() as enemy_scene
-	enemyBody.setEnemy(enemies)	
-	add_child(enemyBody)
+	enemyArray = [
+	load("res://enemy_resources/bruh_enemy.tres"), 
+	load("res://enemy_resources/Up_Enemy.tres"),
+	load("res://enemy_resources/Down_enemy.tres"),
+	load("res://enemy_resources/Left_enemy.tres"),
+	load("res://enemy_resources/Right_enemy.tres"),
+	load("res://enemy_resources/Bean.tres")]
+#	var enemies := load("res://enemy_resources/bruh_enemy.tres") as enemy
+#	var enemyBody = battleSet.instantiate() as enemy_scene
+#	enemyBody.setEnemy(enemies)	
+#	add_child(enemyBody)
+#
+#	enemies = load("res://enemy_resources/Bean.tres")
+#	enemyBody = battleSet.instantiate() as enemy_scene
+#	enemyBody.setEnemy(enemies)	
+#	add_child(enemyBody)
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func generateEnemy(index):
+	var enemyToSpawn := enemyArray[index] as enemy
+	var enemyscene = battleSet.instantiate() as enemy_scene
+	
+	enemyscene.name = enemyToSpawn.name
+	enemyscene.setEnemy(enemyToSpawn)
+	$EnemyUI/Enemies.add_child(enemyscene)
+
+
+func _on_button_pressed():
+	var rng = RandomNumberGenerator.new()
+	generateEnemy(rng.randi_range(0,5))
--- Scenes_Scripts/enemy_scene.gd
+++ Scenes_Scripts/enemy_scene.gd
@@ -1,5 +1,5 @@
 class_name enemy_scene
-extends Node2D
+extends VBoxContainer
 
 var enemy_stats:enemy = null
 
@@ -13,7 +13,6 @@ func _ready():
 	print(enemy_stats.name)
 	print(enemy_stats.health)
 	$Enemy_Texture.texture = enemy_stats.texture
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
